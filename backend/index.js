83ddae6c0b42120710c41343857bcaeeff5211c5
V Pavan Kumar <pavanvalmiki9959@gmail.com>
2025-03-22 20:19:56 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -10,16 +10,10 @@ require('dotenv').config()
 
 
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-});
+app.use(cors({
+    origin: 'https://mychatapp9.netlify.app',
+    credentials: true,
+  }));
 
 app.use(express.json())
 
